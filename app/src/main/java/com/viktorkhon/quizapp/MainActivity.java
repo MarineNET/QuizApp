63bd77fb4e07671f5cd16f11d9442b25c7642974
Viktor Khon <betepokus@yahoo.com>
2017-06-29 12:17:34 -0400

    Add logic to accept answer as a string, check for answer, and onClick on Submit button

--- app/src/main/java/com/viktorkhon/quizapp/MainActivity.java
+++ app/src/main/java/com/viktorkhon/quizapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.viktorkhon.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             submitButton();
+            }
+        });
     }
+
+    int rightAnswer = 0;
+    int wrongAnswer = 0;
+
+    public void submitButton () {
+        if(displayAnswer1()) {
+            rightAnswer++;
+        } else {
+            wrongAnswer++;
+        }
+        Toast.makeText(this,
+                "You have " + rightAnswer + " correct and " +
+                        wrongAnswer +" incorrect answers"
+                , Toast.LENGTH_LONG).show();
+    }
+
+    private boolean displayAnswer1() {
+        EditText answer1 = (EditText) findViewById(R.id.question1);
+        String answer = answer1.getText().toString().toLowerCase();
+        if(answer.equals("brazil")) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }

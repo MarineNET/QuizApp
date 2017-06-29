0db303a8a34cc020c590de7f74fd8930d0f17c06
Viktor Khon <betepokus@yahoo.com>
2017-06-29 13:20:17 -0400

    Add switch statement for radio buttons and check for correct answer

--- app/src/main/java/com/viktorkhon/quizapp/MainActivity.java
+++ app/src/main/java/com/viktorkhon/quizapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
     int wrongAnswer = 0;
 
     public void submitButton () {
-        if(displayAnswer1()) {
+        if (answerForQuestion1()) {
+            rightAnswer++;
+        } else {
+            wrongAnswer++;
+        }
+        if (answerForQuestion2()) {
             rightAnswer++;
         } else {
             wrongAnswer++;
@@ -38,15 +45,39 @@ public class MainActivity extends AppCompatActivity {
                 , Toast.LENGTH_LONG).show();
     }
 
-    private boolean displayAnswer1() {
-        EditText answer1 = (EditText) findViewById(R.id.question1);
+    private boolean answerForQuestion1() {
+        EditText answer1 = (EditText) findViewById(R.id.answer1);
         String answer = answer1.getText().toString().toLowerCase();
-        if(answer.equals("brazil")) {
+        if (answer.equals("brazil")) {
             return true;
         }
         return false;
     }
 
+    public void radioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.radioSwitzerland:
+                if (checked)
+                    break;
+            case R.id.radioEngland:
+                if (checked)
+                    break;
+            case R.id.radioFrance:
+                if (checked)
+                    break;
+            case R.id.radioNorway:
+                if (checked)
+                    break;
+        }
+    }
 
+    public boolean answerForQuestion2() {
+        RadioButton answer2 = (RadioButton) findViewById(R.id.radioSwitzerland);
+        if (answer2.isChecked()) {
+            return true;
+        }
+        return false;
+    }
 
 }
